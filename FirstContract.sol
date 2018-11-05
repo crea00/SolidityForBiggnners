23f7e29803169221829ec97831d7cfdcb0e387ae
Jerome <654321su@gmail.com>
2018-11-05 09:50:55 +0900

    #03 Smart Contract interaction and specific keywords

--- FirstContract.sol
+++ FirstContract.sol
@@ -1,6 +1,27 @@
 pragma solidity ˆ0.4.0;
 
-contract MyFirstContract {
+contract Bank {
+  uint private value;
+
+  // Constructor
+  function Bank(uint amount) public {
+    value = amount;
+  }
+
+  function deposit(uint amount) public {
+    value += amount;
+  }
+
+  function withdraw(uint amount) public {
+    value -= amount;
+  }
+
+  function balance() public constant returns(uint) {
+    return value;
+  }
+}
+
+contract MyFirstContract is Bank(100) {
   string private myname;
   uint private myage;
 
